9919d145621167bc1d8a18cfe112d08f7ef137c9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-23 21:41:58 -0700

    added the useful comments, for the better understandings

--- src/BrothersFromDifferentRoots.java
+++ src/BrothersFromDifferentRoots.java
@@ -14,7 +14,9 @@ class TreeNode{
 }
 
 public class BrothersFromDifferentRoots {
-    static int count;
+    //get a universal count to be used in various methods
+    static int count ;
+    //add the nodes of first tree in the map recursively
     static void addRoot1(TreeNode root1, HashMap<Integer, Boolean> map){
         if(root1 == null) return;
 
@@ -22,6 +24,8 @@ public class BrothersFromDifferentRoots {
         addRoot1(root1.left, map);
         addRoot1(root1.right, map);
     }
+    //now get a difference and compare it with the data present in the tree created above
+    //if yes then increment the count, and repeat the process with left and right side
     static void checkRoot2(TreeNode root2, HashMap<Integer, Boolean> map, int target){
         if(root2 == null) return;
 
@@ -32,10 +36,13 @@ public class BrothersFromDifferentRoots {
         checkRoot2(root2.left, map, target);
         checkRoot2(root2.right, map, target);
     }
+
+    //implement the above two methods in reality
     static int brothersFromDifferentRoots(TreeNode root1, TreeNode root2, int target){
         HashMap<Integer, Boolean> map  = new HashMap<>();
 
         addRoot1(root1, map);
+        //initialize the count as 0, idk why we need to repeat it, even when we could do it when we declared it
         count = 0;
         checkRoot2(root2, map, target);
         return count;
