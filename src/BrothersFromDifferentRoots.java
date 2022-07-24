ee887870e2516d1205060e07eb230df65ceb69a8
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-23 21:22:47 -0700

    coded a method to add  all the elements in the hashmap of one of the trees

--- src/BrothersFromDifferentRoots.java
+++ src/BrothersFromDifferentRoots.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 class TreeNode{
     int data;
@@ -12,6 +13,13 @@ class TreeNode{
 
 public class BrothersFromDifferentRoots {
 
+    static void addRoot1(TreeNode root1, HashMap<Integer, Boolean> map){
+        if(root1 == null) return;
+
+        map.put(root1.data, true);
+        addRoot1(root1.left, map);
+        addRoot1(root1.right, map);
+    }
 
 
     public static void main(String[] args) {
