3d6225da80e7e9cd45c9f61de4e20404fb0d8316
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-23 21:37:28 -0700

    coded another method to check for the presence of the difference in the map

--- src/BrothersFromDifferentRoots.java
+++ src/BrothersFromDifferentRoots.java
@@ -12,13 +12,31 @@ class TreeNode{
 }
 
 public class BrothersFromDifferentRoots {
-
+    static int count;
     static void addRoot1(TreeNode root1, HashMap<Integer, Boolean> map){
         if(root1 == null) return;
 
         map.put(root1.data, true);
         addRoot1(root1.left, map);
         addRoot1(root1.right, map);
+    }
+    static void checkRoot2(TreeNode root2, HashMap<Integer, Boolean> map, int target){
+        if(root2 == null) return;
+
+        int difference = target - root2.data;
+        if(map.containsKey(difference)){
+            count ++;
+        }
+        checkRoot2(root2.left, map, target);
+        checkRoot2(root2.right, map, target);
+    }
+    static int brothersFromDifferentRoots(TreeNode root1, TreeNode root2, int target){
+        HashMap<Integer, Boolean> map  = new HashMap<>();
+
+        addRoot1(root1, map);
+        count = 0;
+        checkRoot2(root2, map, target);
+        return count;
     }
 
 
